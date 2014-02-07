411a30105f78023221e0a00df8647f82f6d261da
KiwiStrongis <kiwistrongis@gmail.com>
2014-02-06 19:21:14 -0500

    working on the tutorial - almost done

--- lists.mk
+++ lists.mk
@@ -2,4 +2,5 @@
 #files list
 class_files = \
 	bin/kiwi/json/JSONAdapter.class \
-	bin/kiwi/json/test/TestJSONAdapter.class
+	bin/kiwi/json/test/TestJSONAdapter.class \
+	bin/kiwi/json/tutorial/Tutorial.class \

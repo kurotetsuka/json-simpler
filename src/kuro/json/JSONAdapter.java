1a209e294b8e0a6f9064fe62dbad2650cc2a367d
kurotetsuka <kurotetsuka@gmail.com>
2014-09-28 18:52:28 -0400

    did a bunch of commenting, and a little refactoring

--- src/kuro/json/JSONAdapter.java
+++ src/kuro/json/JSONAdapter.java
@@ -2,6 +2,7 @@ package kuro.json;
 
 //standard library imports
 import java.util.NoSuchElementException;
+import java.util.List;
 import java.util.Vector;
 
 //json-simple imports
@@ -12,19 +13,37 @@ public class JSONAdapter {
 	private Object root;
 
 	//constructors
-	public JSONAdapter( Object object){
+	/**
+	 * Create a new adapter to a json-simple type. These valid types include the following classes and any of their extentions: JSONArray, JSONObject, Boolean, Double, Long, String, null.
+	 * 
+	 * @param object The desired root object of the adapter. Must be a valid json-simple type.
+	 * @throws IllegalArgumentException if the passed object was not a valid json-simple type.
+	 */
+	public JSONAdapter( Object root){
 		//type check
-		if( isJSONSimpleType( object))
-			root = object;
+		if( isJSONSimpleType( root))
+			this.root = root;
 		//throw error
 		else throw new IllegalArgumentException(
 			"JSONAdapter passed an object that was not a json-simple type");}
 
 	//get functions
 	//raw get
+	/**
+	 * Get the root object of this adapter.
+	 * @return the root object of this adapter
+	 */
 	public Object get(){
 		return root;}
+
 	//array get
+	/**
+	 * Get an element from this adapter's root object. The root object must be a json-simple array.
+	 * 
+	 * @param index the index of the desired element
+	 * @return the element at the desired index
+	 * @throws NoSuchElementException if the root object does not contain the desired element ( this may be due to the root object not being a json array )
+	 */
 	public JSONAdapter get( int index){
 		//validation
 		if( ! this.isJSONArray())
@@ -37,7 +56,15 @@ public class JSONAdapter {
 		Object result = ( (JSONArray) root).get( index);
 		//create and return adapter for desired object
 		return new JSONAdapter( result);}
+
 	//object get
+	/**
+	 * Get a field from this adapter's root object. The root object must be a json-simple object.
+	 * 
+	 * @param tag the field key of the desired field
+	 * @return the value of desired field
+	 * @throws NoSuchElementException if the root object does not contain the desired field ( this may be due to the root object not being a json object )
+	 */
 	public JSONAdapter get( String tag){
 		//validation
 		if( ! this.isJSONObject())
@@ -50,13 +77,24 @@ public class JSONAdapter {
 		Object result = ( (JSONObject) root).get( tag);
 		//create and return adapter for desired object
 		return new JSONAdapter( result);}
+
  	//multi-level object get
+ 	/**
+ 	 * Get a nested field from this adapter's root object. The root object and every child along the path must be json-simple objects.
+ 	 *
+ 	 * This function is simply a shortcut for doing multiple gets in one call. Example: <code>adapter.get("foo").get("bar")</code> is congruent to <code>adapter.get( new String[]{"foo", "bar"})</code>.
+ 	 * 
+ 	 * @param tags the path to the desired object
+ 	 * @return an adapter for the object at the desired path
+	 * @throws IllegalArgumentException if the tags array is empty or null
+	 * @throws NoSuchElementException if the desired object cannot be found
+ 	 */
 	public JSONAdapter get( String[] tags){
 		//validation
 		if( ! this.isJSONObject())
 			throw new NoSuchElementException(
 				"JSONAdapter root object is not a json object");
-		if( tags.length == 0)
+		if( tags == null || tags.length == 0)
 			throw new IllegalArgumentException(
 				"Tags array must not be empty");
 		//walk through the object hierarchy
@@ -68,6 +106,13 @@ public class JSONAdapter {
 
 	//set functions
 	//array set
+	/**
+	 * Change an element of this adapter's root object. The root object must be a json-simple array.
+	 * 
+	 * @param index the index of the element that should be changed
+	 * @param value the new desired value of the element
+	 * @return the previous value of the changed element
+	 */
 	public Object set( int index, Object value){
 		//validation
 		if( ! this.isJSONArray())
@@ -81,6 +126,13 @@ public class JSONAdapter {
 		return array.set( index, value);}
 
 	//object set
+	/**
+	 * Change a field of this adapter's root object. The root object must be a json-simple object.
+	 * 
+	 * @param tag the key of the field that should be changed
+	 * @param value the new desired value of the field
+	 * @return the previous value of the changed field, or null if the field did not previously exist ( null can also be the previous value of the field )
+	 */
 	public Object set( String tag, Object value){
 		//validation
 		if( ! this.isJSONObject())
@@ -92,7 +144,15 @@ public class JSONAdapter {
 		//apply the set
 		JSONObject object = this.getJSONObject();
 		return object.put( tag, value);}
+
 	//multi-level object set
+	/**
+	 * Change a nested field of this adapter's root object. The root object and every child along the path must be json-simple objects.
+	 * 
+	 * @param tags the path to the field that should be changed
+	 * @param value the new desired value of the field
+	 * @return the previous value of the changed field, or null if the field did not previously exist ( null can also be the previous value of the field )
+	 */
 	public Object set( String[] tags, Object value){
 		//validation
 		if( ! this.isJSONObject())
@@ -129,6 +189,23 @@ public class JSONAdapter {
 		return adapter.set( last_tag, value);}
 
 	//deref functions
+	/**
+	 * Dereference an object, javascript-style.
+	 * 
+	 * Examples:
+	 * <ul>
+	 * <li><code>adapter.deref(".subfield[0]")</code> or <code>adapter.deref("subfield[0]")</code> would be equivalent to <code>adapter.get("subfield").get(0)</code></li>
+	 * <li>"[0].asdf.foo[1]"</li>
+	 * <li>"[0][1][2]"</li>
+	 * </ul>
+	 * Check out the tutorial for more examples.
+	 * 
+	 * @param reference a javascript-style dereference string for the desired object
+	 * @return the object pointed to by the given reference, if it exists.
+	 * @throws NullPointerException if the given reference string is null
+	 * @throws IllegalArgumentException if the given reference string cannot be parsed
+	 * @throws NoSuchElementException if no object can be found at the given reference
+	 */
 	public JSONAdapter deref( String reference){
 		//validation
 		if( reference == null)
@@ -155,6 +232,11 @@ public class JSONAdapter {
 		return adapter;}
 
 	//contains functions
+	/**
+	 * Check whether the given index is valid for this adapter's root object.
+	 * @param index the index to check
+	 * @return true if this adapter's root object is an array and can contain the given index, false otherwise
+	 */
 	public boolean containsIndex( int index){
 		//validate type
 		if( this.isJSONArray()){
@@ -162,6 +244,12 @@ public class JSONAdapter {
 			return index >= 0 && index < array.size();}
 		//we're not an array, so we cannot contain an index
 		else return false;}
+
+	/**
+	 * Check whether the given key is valid for this adapter's root object.
+	 * @param key the key to check
+	 * @return true if this adapter's root object is a json object and contains the given key, false otherwise
+	 */
 	public boolean containsKey( String key){
 		//validate type
 		if( this.isJSONObject()){
@@ -169,6 +257,12 @@ public class JSONAdapter {
 			return object.containsKey( key);}
 		//we're not an object, so we cannot contain a key
 		else return false;}
+
+	/**
+	 * Check whether the given value is mapped to by this adapter's root object.
+	 * @param value the value to check for
+	 * @return true if this adapter's root object is a json object or array and contains the given value, false otherwise
+	 */
 	public boolean containsValue( Object value){
 		//we're an object
 		if( this.isJSONObject()){
@@ -182,20 +276,52 @@ public class JSONAdapter {
 		else return false;}
 
 	//type check functions
+	/**
+	 * Check if this adapter's root object is a boolean object.
+	 * @return true if this adapter's root object is a boolean object.
+	 */
 	public boolean isBoolean(){
 		return root instanceof Boolean;}
+	/**
+	 * Check if this adapter's root object is a float or double object.
+	 * @return true if this adapter's root object is a float or double.
+	 */
 	public boolean isDecimal(){
 		return root instanceof Double;}
+	/**
+	 * Check if this adapter's root object is an integer object.
+	 * @return true if this adapter's root object is an integer.
+	 */
 	public boolean isInteger(){
 		return root instanceof Long;}
+	/**
+	 * Check if this adapter's root object is null.
+	 * @return true if this adapter's root object is null.
+	 */
 	public boolean isNull(){
 		return root == null;}
+	/**
+	 * Check if this adapter's root object is a integer or long object.
+	 * @return true if this adapter's root object is a integer or long.
+	 */
 	public boolean isNumber(){
 		return root instanceof Number;}
+	/**
+	 * Check if this adapter's root object is a string object.
+	 * @return true if this adapter's root object is a string.
+	 */
 	public boolean isString(){
 		return root instanceof String;}
+	/**
+	 * Check if this adapter's root object is a json array object.
+	 * @return true if this adapter's root object is a json array.
+	 */
 	public boolean isJSONArray(){
 		return root instanceof JSONArray;}
+	/**
+	 * Check if this adapter's root object is a json object.
+	 * @return true if this adapter's root object is a json object.
+	 */
 	public boolean isJSONObject(){
 		return root instanceof JSONObject;}
 
@@ -525,6 +651,11 @@ public class JSONAdapter {
 		return adapter.getObject();}
 
 	//utility functions
+	/**
+	 * Check if the given object is a json simple type
+	 * @param object the object to check
+	 * @return true if the given object is a json simple type, false otherwise
+	 */
 	public static boolean isJSONSimpleType( Object object){
 		 return (
 	 		( object == null) ||
@@ -537,10 +668,18 @@ public class JSONAdapter {
 
 	//accessors
 	@Override
+	/**
+	 * Return a string representation of this adapter's root object.
+	 * @return a string representation of this adapter's root object
+	 */
 	public String toString(){
 		return root == null ?
 			"null" : root.toString();}
 
+	/**
+	 * Get the size of this adapter's root object. The root object must be a json array or object type.
+	 * @return if the root object is an array, the size of the array, if it is an object, the number of entries in it's map.
+	 */
 	public int size(){
 		//validate type
 		if( this.isJSONArray())
@@ -558,11 +697,11 @@ public class JSONAdapter {
 	 * Just a little utility class to make dereferencing easier
 	 */
 	public static class Token {
-		//whether this token is a name
+		/** whether this token is a name */
 		private boolean symbol;
-		//the name of this token
+		/** the name of this token */
 		public String name;
-		//the index of this token
+		/** the index of this token */
 		public int index;
 
 		//constructors
@@ -580,11 +719,6 @@ public class JSONAdapter {
 			return symbol;}
 		public boolean isIndex(){
 			return ! symbol;}
-
-		public String toString(){
-			return String.format(
-				symbol ? ".%s" : "[%d]",
-				symbol ? name : index);}
 
 		//utility function
 		public static Vector<Token> parse( String reference){
@@ -661,12 +795,33 @@ public class JSONAdapter {
 			return result;}
 
 		//string casts
-		public static String toString( Vector<Token> tokens){
+		/**
+		 * Create a string representation of this token.
+		 * @return a string representation of this token
+		 */
+		public String toString(){
+			return String.format(
+				symbol ? ".%s" : "[%d]",
+				symbol ? name : index);}
+
+		/**
+		 * Create a string representation of a token path.
+		 * @param tokens the token path to be stringified
+		 * @return a string representation of the given token path
+		 */
+		public static String toString( List<Token> tokens){
 			String result = new String();
 			for( Token token : tokens)
 				result += token.toString();
 			return result;}
-		public static String toString( Vector<Token> tokens, int end){
+
+		/**
+		 * Create a string representation of the subset of a token path.
+		 * @param tokens the token path to be stringified
+		 * @param end the exclusive end index of the token path in the list
+		 * @return a string representation of the given token path
+		 */
+		public static String toString( List<Token> tokens, int end){
 			String result = new String();
 			for( int i = 0; i < end && i < tokens.size(); i++)
 				result += tokens.get(i).toString();
